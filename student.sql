4ea19fe4baab2cb7f77d468cb6e0cd690305f7a4
Praveena Vyas <praveena.vyas@bcah.christuniversity.in>
2025-12-10 15:09:31 +0530

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,6 @@
+create table students (s_id int primary key, s_name varchar(50), s_course varchar(30));
+
+insert into student values(4, "Praveena", "BCA"),
+(22, "Saakshi", "Psychology"), 
+(43, "Praveena", "BCA"),
+(52, "Medha", "Bsc";
