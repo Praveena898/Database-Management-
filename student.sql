379d896604214aa857702fc7d48b3e892d69bcc1
Praveena Vyas <praveena.vyas@bcah.christuniversity.in>
2025-12-10 15:22:58 +0530

    Update student.sql

--- student.sql
+++ student.sql
@@ -3,4 +3,6 @@ create table students (s_id int primary key, s_name varchar(50), s_course varcha
 insert into student values(4, "Praveena", "BCA"),
 (22, "Saakshi", "Psychology"), 
 (43, "Praveena", "BCA"),
-(52, "Medha", "Bsc";
+(52, "Medha", "Bsc"),
+(12,"Nikhil","Bsc"),
+(13,"Yati", "BBA");
